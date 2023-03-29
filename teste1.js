d431b1324a8074cf0b733ccc0bcbf976ca0e5aae
KarlaMag <64945344+karlamagueta@users.noreply.github.com>
2023-03-29 16:59:42 +0100

    Add files via upload

---
+++ teste1.js
@@ -0,0 +1,11 @@
+const nome = ' O Senhor dos Anéis';
+const ano = 1954;
+const autor = 'J. R. R. Tolkien';
+
+console.log (nome + ' por ' + autor);
+
+function livro() {
+    console.log (nome.toUpperCase)
+    console.log (2023 - ano)
+    console.log (nome + ' por ' + autor)   
+}
