e0247e48629ba3435faa68d429ba0d5ff4e2b158
KarlaMag <64945344+karlamagueta@users.noreply.github.com>
2023-03-30 15:49:09 +0100

    Add files via upload

--- teste1.js
+++ teste1.js
@@ -1,11 +1,25 @@
-const nome = ' O Senhor dos Anéis';
-const ano = 1954;
-const autor = 'J. R. R. Tolkien';
-
-console.log (nome + ' por ' + autor);
-
-function livro() {
-    console.log (nome.toUpperCase)
-    console.log (2023 - ano)
-    console.log (nome + ' por ' + autor)   
+function livro (nome, ano, autor) {
+    resultado_livro = {
+        nome_upper: nome.toUpperCase(),
+        ano: 2023 - ano,
+        frase: nome + ' por ' + autor,
+    }
+    return resultado_livro;
 }
+
+resultado = livro ('O Senhor dos Aneis', 1984, 'J R R Tolkien')
+console.log (resultado)
+
+/* function checkout(produto, quantidade) {
+    const total = produto.preco * quantidade;
+    return total;
+}
+
+const celular = {
+    preco: 1000,
+}
+
+total = checkout(celular, 2);
+console.log(total)
+ */
+
